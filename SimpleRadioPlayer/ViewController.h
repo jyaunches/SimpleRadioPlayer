bc41c698d8aaaa9b7ebb6d1aaa7dd1a44f3f7aef
Julie Yaunches <jmyaunch@gmail.com>
2013-10-28 13:35:52 -0400

    Initial Commit

---
+++ SimpleRadioPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleRadioPlayer
+//
+//  Created by Julietta Yaunches on 28/10/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SimpleRadioPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleRadioPlayer
+//
+//  Created by Julietta Yaunches on 28/10/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
